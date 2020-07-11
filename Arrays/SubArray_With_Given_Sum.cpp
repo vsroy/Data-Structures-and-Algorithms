867d5a1700c197a7b3b5c55897d094d552fe83c6
vsroy <vishalshillong258@gmail.com>
2020-07-11 10:16:41 +0530

    Adding the problem statement of the program

--- Arrays/SubArray_With_Given_Sum.cpp
+++ Arrays/SubArray_With_Given_Sum.cpp
@@ -1,3 +1,5 @@
+/*Given an unsorted array A of size N of non-negative integers, find a continuous sub-array which adds to a given number S.*/
+
 #include<iostream>
 #include<vector>
 using namespace std;
